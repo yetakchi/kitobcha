fb429eb59245e629c2a2d4f1341e6ba69885d517
Anvar Zaripboyev <58693223+Anvar-Avenger@users.noreply.github.com>
2023-08-15 12:25:52 +0500

    Yuklandi

---
+++ README.md
@@ -0,0 +1,15 @@
+# Kitobcha
+Kitob betlarini sahifalash uchun oddiy dastur!
+
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://github.com/Anvar-Avenger/kitobcha/blob/main/LICENSE)
+
+
+## Imkoniyatlari
+* Kiritilsan sahifalarga mos ravishta kitobcha uchun sahifalar juftligini hosil qilish
+* Ikki tomonlama (Oldi-Orqa) shaklida sahifalar juftligini hosil qilish
+
+## Foydalanilgan manbalar:
+* [Scheme](https://en.wikipedia.org/wiki/Schema.org) - Scheme is a classic programming language in the Lisp family. 
+
+## Havolalar
+* [Kitobcha](https://kitobcha.joriy.uz/) - Ishlatish uchun
---
+++ hodisa.js
@@ -0,0 +1,157 @@
+const chiqar = document.querySelector("#chiqar");
+const boshi = document.getElementById("boshi");
+const oxiri = document.getElementById("oxiri");
+
+// Tanlov
+const SHAKL_KITOB = 1;
+const SHAKL_OLDI_ORQA = 2;
+
+// const shakl = document.getElementsByName("shakl");
+const kitob = document.getElementById("kitob");
+const ikkiTomonlama = document.getElementById("ikki-tomonli");
+
+// Chiquvchilar
+const old = document.getElementById("old");
+const orqa = document.getElementById("orqa");
+
+/**
+ * @var x - Boshlanish
+ * @var y - Tugash
+ */
+let x, y;
+
+
+/**
+ * @param
+ * @author Anvar Zaripboyev
+ * Chiquvchi maydonlarni tozalash
+ */
+function maydonniTozala() {
+    old.value = '';
+    orqa.value = '';
+}
+
+function tekshir(qiymat, qaytaAloqa) {
+    for (let i = 0; i < qiymat.length; i++) {
+        if (qiymat[i] >= '0' && qiymat[i] <= '9') {
+            continue;
+        } else {
+            qaytaAloqa();
+            break;
+        }
+    }
+}
+
+/**
+ * Kitobcha shaklida
+ * @param b - Boshlanish
+ * @param t - Tugashi
+ */
+function shaklKitobcha(b, t) {
+    /**
+     * m Markaz, O'rta
+     */
+    let m = t - b + 1;
+
+    let a = [];
+    for (let i = 1; i <= m; i++) {
+        a[i] = (b + i) - 1;
+    }
+
+    if (m % 4 == 0) {
+        let son = m / 4;
+
+        for (let i = m; i >= 1; i--) {
+            if (i % 2 == 0) {
+                if (son > 0) {
+                    old.value += (a[i] + "," + (a[m - i + 1]) + (m / 2 + 2 == i ? "" : ","));
+                } else {
+                    orqa.value += (a[i] + "," + (a[m - i + 1]) + (i == 2 ? "" : ","));
+                }
+                son--;
+            }
+
+            console.log(a[i] + "," + (a[m - i + 1]) + ", " + i + " ");
+        }
+        alert("Jami: " + m / 4 + " varaq");
+    } else {
+        alert("Kitob shaklida chop etish uchun sahifalar soni \n" + "to\u2018rtga qoldiqsiz bo\u2018linuvchi bo\u2018lishi kerak!");
+    }
+}
+
+/* 
+ * Ikki tomonlama shakli
+ * @param b - Boshlanish raqami
+ * @param t - Tugash raqami
+ */
+function shaklIkkiTomonlama(b, t) {
+    // m - Markaz
+    let m = t - b + 1;
+
+    if (m % 2 == 0) {
+        for (let i = b; i <= t; i += 2) {
+            old.value += (i + (i == t - 1 ? "" : ","));
+        }
+        for (let i = t; i >= b; i -= 2) {
+            orqa.value += (i + (i == b + 1 ? "" : ","));
+        }
+    } else {
+        alert("Sahifalar soni juft bo\u2018lishi kerak!");
+    }
+}
+
+
+kitob.onchange = maydonniTozala;
+ikkiTomonlama.onchange = maydonniTozala;
+
+boshi.addEventListener("keyup", function (keyboardEvent ) {
+    var input = keyboardEvent.target;
+    let qiymat = input.value;
+    let son = true;
+    tekshir(qiymat, function () {
+        son = false;
+        alert("Faqat raqam kiriting");
+    });
+
+    if (son) {
+        x = parseInt(qiymat);
+    }
+
+    maydonniTozala();
+});
+
+oxiri.addEventListener("keyup", function (keyboardEvent ) {
+    var input = keyboardEvent.target;
+    let qiymat = input.value;
+    let son = true;
+    tekshir(qiymat, function () {
+        son = false;
+        alert("Faqat raqam kiriting");
+    });
+
+    if (son) {
+        y = parseInt(qiymat);
+    }
+
+    maydonniTozala();
+});
+
+chiqar.onclick = function () {
+    maydonniTozala();
+
+    if (x * y > 0) {
+        if (kitob.checked) {
+            shaklKitobcha(x, y);
+        }
+
+        if (ikkiTomonlama.checked) {
+            shaklIkkiTomonlama(x, y);
+        }
+    } else {
+        alert("Ma\u2019lumot kiritlmagan!");
+    }
+}
+
+
+
+
